96de7d5282d8126ae908dd1efe782c6e5c752963
jerrytc7 <jerrykondner@gmail.com>
2022-02-07 17:44:33 -0500

    adding static data store

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
+const shopItems = [
+  {
+    id: 1,
+    name: "Potato",
+    price: 5
+  },
+  {
+    id: 2,
+    name: "Banana",
+    price: 4
+  },
+  {
+    id: 3,
+    name: "Apple",
+    price: 7
+  },
+  {
+    id: 4,
+    name: "Orange",
+    price: 2
+  },
+  {
+    id: 5,
+    name: "Blueberries",
+    price: 9
+  },
+]
 
 function App() {
   return (
     <div className="App">
-        
+
     </div>
   );
 }
