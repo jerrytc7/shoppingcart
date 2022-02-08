557fe0d22b7f97c5de7ee595149c336143666ff3
jerrytc7 <jerrykondner@gmail.com>
2022-02-07 19:01:11 -0500

    added add to cart helper function

--- src/App.js
+++ src/App.js
@@ -28,8 +28,55 @@ const shopItems = [
   },
 ]
 
+/*
+cartItems = [
+  {
+    id: 1,
+    count: 3
+  },
+  {
+    id: 2,
+    count: 2
+  }
+]
+*/
+
+/*
+  cartItems = [
+
+  ]
+*/
+
 function App() {
   const [cartItems, setCartItems] = useState([])
+
+  const addToCart = (id) => {
+    const existingItem = cartItems.some((cartItem)=>{
+      return cartItem.id === id
+    })
+    /*const count = !existingItem ? 1 : existingItem.count + 1
+    if(!existingItem){
+      count = 1
+    }else{
+      existingItem.count + 1
+    }*/
+    setCartItems(!existingItem ? [
+      ...cartItems,
+      {
+        id: id,
+        count: 1
+      }
+    ] : cartItems.map((cartItem)=>{
+        if(cartItem.id === id){
+          return {
+            id: id,
+            count: cartItem.count + 1
+          }
+        }else{
+          return cartItem
+        }
+    }))
+  }
 
   return (
     <div className="App">
